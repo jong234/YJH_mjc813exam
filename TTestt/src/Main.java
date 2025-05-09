193037eed9647029dfee4dee21adf8b4ab6ab31c
jong234 <oip9663@naver.com>
2025-05-09 21:46:49 +0900

    already finsh

--- TTestt/src/Main.java
+++ TTestt/src/Main.java
@@ -11,3 +11,41 @@ public class Main {
         System.out.println(answer);
     }
 }
+class Main2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if(i % 3 == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
+class Main3 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        while (a + b < 100) {
+           a = (int) (Math.random() * 6 + 1);
+           b = (int) (Math.random() * 6 + 1);
+           if (a + b  == 5) {
+               System.out.println(a + "," + b);
+               break;
+           }
+           System.out.println(a + "," + b);
+        }
+    }
+}
+class Main4 {
+    public static void main(String[] args) {
+
+        for (int x = 0; x <= 10; x++) {
+            for (int y = 0; y <= 10; y++) {
+                if(4 * x + 5 * y == 60 ){
+                    System.out.println(x + "," + y);
+                }
+            }
+        }
+    }
+}
