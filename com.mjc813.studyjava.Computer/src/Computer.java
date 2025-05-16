0335da769e34f138220768f799e7cb63fc7779f9
jong234 <oip9663@naver.com>
2025-05-16 20:13:24 +0900

    final

--- com.mjc813.studyjava.Computer/src/Computer.java
+++ com.mjc813.studyjava.Computer/src/Computer.java
@@ -6,53 +6,44 @@ public class Computer {
     String[] StorgeCapacity = {"500GB","1000GB","2000GB"};
     String[] Graphic = {"GTX_1660","RTX_4080","Intel_A770"};
 
-    Computer(String[] ComName, String[] CPU, String[] Ram, String[] Storage, String[] storgeCapacity , String[] Graphic) {
-        this.ComName = ComName;
-        this.CPU = CPU;
-        this.Ram = Ram;
-        this.Storage = Storage;
-        this.StorgeCapacity = storgeCapacity;
-        this.Graphic = Graphic;
+
+    public void run(int ComName1) {
+            System.out.println(ComName[ComName1]+"가 켜졌습니다.");
     }
 
-
-    public void run() {
-            System.out.println(ComName+"가 켜졌습니다.");
+    public void show(int ComName1, int CpuName, int RamName, int StorageName, int StorgeCapacity1, int GraphicName) {
+        System.out.println(ComName[ComName1] +"의 정보입니다.");
+        System.out.println("CPU : " + CPU[CpuName]);
+        System.out.println("Ram : " + Ram[RamName]);
+        System.out.println("Storage : " + Storage[StorageName]);
+        System.out.println("StorgeCapacity : " + StorgeCapacity[StorgeCapacity1]);
+        System.out.println("Graphic : " + Graphic[GraphicName]);
     }
 
-    public void show() {
-        System.out.println(ComName +"의 정보입니다.");
-        System.out.println("CPU : " + CPU);
-        System.out.println("Ram : " + Ram);
-        System.out.println("Storage : " + Storage);
-        System.out.println("StorgeCapacity : " + StorgeCapacity);
-        System.out.println("Graphic : " + Graphic);
-    }
-
-    public void end() {
-            System.out.println(ComName+"가 꺼집니다.");
+    public void end(int ComName1) {
+            System.out.println(ComName[ComName1]+"가 꺼집니다.\n");
     }
     public static void main(String[] args) {
-        Computer com = new Computer(new String[1], new String[2], new String[2], new String[1], new String[2], new String[2]);
-        com.run();
-        com.show();
-        com.end();
-        Computer com2 = new Computer(new String[2], new String[3], new String[4], new String[1], new String[1], new String[2]);
-        com2.run();
-        com2.show();
-        com2.end();
-        Computer com3 = new Computer(new String[3], new String[1], new String[4], new String[1], new String[2], new String[2]);
-        com3.run();
-        com3.show();
-        com3.end();
-        Computer com4 = new Computer(new String[4], new String[2], new String[1], new String[2], new String[2], new String[2]);
-        com4.run();
-        com4.show();
-        com4.end();
-        Computer com5 =  new Computer(new String[5], new String[3], new String[4], new String[1], new String[3], new String[1]);
-        com5.run();
-        com5.show();
-        com5.end();
+        Computer com = new Computer();
+        com.run(0);
+        com.show(0, 2, 3 ,1 , 2, 1);
+        com.end(0);
+        Computer com2 = new Computer();
+        com2.run(1);
+        com2.show(1, 1 , 3, 1, 1 ,1);
+        com2.end(1);
+        Computer com3 = new Computer();
+        com3.run(2);
+        com3.show(2, 0 , 1 ,1 ,1 ,1);
+        com3.end(2);
+        Computer com4 = new Computer();
+        com4.run(3);
+        com4.show(3 , 2 , 3 ,0 ,0 ,0);
+        com4.end(3);
+        Computer com5 =  new Computer();
+        com5.run(4);
+        com5.show(4, 2 , 2 , 0, 2 ,2);
+        com5.end(4);
         System.out.println("부팅 종료");
 
     }
