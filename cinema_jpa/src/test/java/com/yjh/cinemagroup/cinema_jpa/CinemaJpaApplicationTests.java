67435378da0a2f617c9d4ff2394122377e476553
jong234 <oip9663@naver.com>
2025-07-31 14:28:19 +0900

    cinema_jpa yml파일 수정

---
+++ cinema_jpa/src/test/java/com/yjh/cinemagroup/cinema_jpa/CinemaJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh.cinemagroup.cinema_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
